409377a853ca878f949d09b2984231f409ba99a7
Juri Fedjaev <j.fedjaev@gmail.com>
2017-04-10 10:03:32 +0200

    function to set class and trial onset times

---
+++ Code/Biopac/Matlab/getClassLabels.m
@@ -0,0 +1,11 @@
+%% This function sets the onset time of trials and classes
+%  Author:  Juri Fedjaev
+%  Last modified: 10//04/17
+
+function [labels] = getClassLabels(data)
+sz = length(data.X);
+
+
+
+
+end
